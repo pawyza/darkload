bf428fa3ccb46dd246a521b61a83a92ff46a6009
mrqubes <111229001+mrqubes@users.noreply.github.com>
2024-01-14 19:46:41 +0100

    WIP

--- enemy.gd
+++ enemy.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
 @export var player : CharacterBody2D
-var speed=Vector2(100,0)
+@export var stun_time = 1.5
+var speed=Vector2(210,0)
 
+var stunned = false
 
 func _physics_process(delta):
-
+	check_if_stunned()
 	follow_player()
 	move_and_slide()
 	
@@ -14,10 +16,17 @@ func get_player_position():
 	return player.position
 	
 func follow_player():
-	if player.flashlight_state:
-		velocity = Vector2.ZERO
+	if stunned:
+		velocity.x = move_toward(velocity.x, 0, 10)
+		velocity.y = move_toward(velocity.y, 0, 10)
 	else:
 		var rotated_speed = speed.rotated(position.angle_to_point(player.position))
 		velocity = rotated_speed
 
+func check_if_stunned():
+	if player.flashlight_state and player.check_if_in_light(self):
+		$StunTimer.start(stun_time)
+		stunned = true
 
+func _on_stun_timer_timeout():
+	stunned = false

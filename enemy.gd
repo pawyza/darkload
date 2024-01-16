32dffaaf00e453ceb60f21b75a426185c415ec5c
Filip Pawyza <pawyza.f@gmail.com>
2024-01-16 20:28:49 +0100

    Pathfinding + sprites

--- enemy.gd
+++ enemy.gd
@@ -2,27 +2,47 @@ extends CharacterBody2D
 
 @export var player : CharacterBody2D
 @export var stun_time = 1.5
-var speed=Vector2(210,0)
+@export var movement_speed = 210
+@onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
 
 var stunned = false
+
+func _ready():
+	navigation_agent.path_desired_distance = 4.0
+	navigation_agent.target_desired_distance = 4.0
+	call_deferred("actor_setup")
+
+var i = 0
 
 func _physics_process(delta):
 	check_if_stunned()
 	follow_player()
 	move_and_slide()
-	
-func get_player_position():
-	
-	return player.position
+	if i < 10:
+		i += 1
+		actor_setup()
+	else:
+		i = 0
+
+func actor_setup():
+	await get_tree().physics_frame
+	set_movement_target(player.position)
+
+func set_movement_target( movement_target ):
+	navigation_agent.target_position = movement_target
 	
 func follow_player():
-	print(position.distance_squared_to(player.position)/80)
 	if stunned:
 		velocity.x = move_toward(velocity.x, 0, 20 / (position.distance_to(player.position)/80))
 		velocity.y = move_toward(velocity.y, 0, 20 / (position.distance_to(player.position)/80))
 	else:
-		var rotated_speed = speed.rotated(position.angle_to_point(player.position))
-		velocity = rotated_speed
+		if navigation_agent.is_navigation_finished():
+			return
+		var current_agent_position = global_position
+		var next_path_position = navigation_agent.get_next_path_position()
+		velocity = current_agent_position.direction_to(next_path_position) * movement_speed
+			
+		rotation = lerp_angle(rotation, position.angle_to_point(player.position)+PI, 0.1)
 
 func check_if_stunned():
 	if player.flashlight_state and player.check_if_in_light(self):
